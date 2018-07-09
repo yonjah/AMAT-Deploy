0289f72064c4010b2f626e53f7b41fc037d78ada
Gareth Clarson <gareth_clarson@iinet.net.au>
2018-07-09 14:57:05 +0800

    Add files via upload

--- AutoInstall/MasterInstall/terrafalse.sh
+++ AutoInstall/MasterInstall/terrafalse.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+#Author: Gareth Clarson - Vetriixx & Tarun Patel
+#AutoDeploy Script
 
-./packer build -var-file variables.json MispVM.json
-./packer build -var-file variables.json FameVM.json
-./packer build -var-file variables.json CuckooVM.json
-./packer build -var-file variables.json DionaeaVM.json
-./packer build -var-file variables.json ViperVM.json
-./packer build -var-file variables.json MastiffVM.json
+#Check if script was run as root
+if [ $EUID -ne 0 ]; then
+	echo 'This script must be run as root!'
+	exit 1
+fi
 
-#Turns on machine in order (need to alter autovm script to include all vms once all done)
-sshpass -p "##EnterPassword##" scp ./autovm_poweron.sh ##ESXi Root##@##ESXi IP##:/
-echo -e "./autovm_poweron.sh" | sshpass -p "##EnterPassword##" ssh ##ESXi Root##@##ESXi IP##
+	./packer build -var-file variables.json FameVM.json
+	./packer build -var-file variables.json CuckooVM.json
+	./packer build -var-file variables.json ViperVM.json
+	./packer build -var-file variables.json MastiffVM.json
+	./packer build -var-file variables.json MispVM.json
+	./packer build -var-file variables.json DionaeaVM.json
+	
+	#Test vm for testing (obvs)
+	#./packer build -var-file variables.json test.json
+
+
+
+
+	#Turns on machine in order (need to alter autovm script to include all vms once all done)
+	sshpass -p "JungleButtPirat3s" scp ./autovm_poweron.sh amat@192.168.58.45:~
+	sshpass -p "JungleButtPirat3s" ssh -o StrictHostKeyChecking=no amat@192.168.58.45 'sh autovm_poweron.sh'
+
+		
+exit 0
