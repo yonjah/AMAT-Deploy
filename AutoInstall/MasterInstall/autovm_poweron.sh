1ad44ccd33f061003d74d9df06dfdaa418c5f813
Gareth Clarson <gareth_clarson@iinet.net.au>
2018-07-09 14:56:16 +0800

    Add files via upload

--- AutoInstall/MasterInstall/autovm_poweron.sh
+++ AutoInstall/MasterInstall/autovm_poweron.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Turns on machine in order listed in line 1
 
-for i in misp fame viper cuckoo mastiff dionaea
+for i in misp fame viper cuckoo mastiff dionaea #test
 do
         id="$(vim-cmd vmsvc/getallvms | sed '1d' | awk -v a="$i" '{if ($2 == a) print $1}')"
         vim-cmd vmsvc/power.on "$id"
